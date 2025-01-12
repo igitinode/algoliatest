68ee5dd7e748526b3444d0f65aff3201b8951de6
igitinode <igitinode@gmail.com>
2025-01-12 16:27:19 +0800

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,86 @@
+import { defineConfig } from 'vitepress'
+
+import algolia from './algolia.mjs'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/algoliatest/',
+  title: "algolia-test",
+  description: "Vitepress + Vue + Algolia",
+  lastUpdated: true,
+  cleanUrls: true,
+  metaChunk: true,
+  sitemap: {
+    hostname: 'https://igitinode.github.io/algoliatest',
+    transformItems(items) {
+      return items.filter((item) => !item.url.includes('migration'))
+    }
+  },
+  /* prettier-ignore */
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/algoliatest/vitepress-logo-mini.svg' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/algoliatest/vitepress-logo-mini.png' }],
+    ['meta', { name: 'theme-color', content: '#5f67ee' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: 'Algolia Test | Vite & Vue Powered Static Site Generator' }],
+    ['meta', { property: 'og:site_name', content: 'VitePress' }],
+    ['meta', { property: 'og:image', content: 'https://igitinode.github.io/algoliatest/vitepress-og.jpg' }],
+    ['meta', { property: 'og:url', content: 'https://igitinode.github.io/algoliatest/' }],
+  ],
+  themeConfig: {
+    logo: { src: '/vitepress-logo-mini.svg', width: 24, height: 24 },
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://igitinode.github.io/algoliatest' }
+    ],
+    footer: {
+      message: '基于 MIT 许可发布',
+      copyright: `版权所有 © 2019-${new Date().getFullYear()} 尤雨溪`
+    },
+
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+
+    outline: {
+      label: '页面导航'
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium'
+      }
+    },
+
+    langMenuLabel: '多语言',
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+    skipToContentLabel: '跳转到内容',
+
+    search: {
+      provider: 'local',
+      options: algolia,
+    },
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "algolia-test"
+  text: "Vitepress + Vue + Algolia"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+  image:
+    src: /vitepress-logo-large.webp
+    alt: VitePress
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
